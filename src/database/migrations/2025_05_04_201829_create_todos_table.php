926255e21467e4b7f81c2cbd9bf48f9f7c2da220
ai2525desu <umi08062525@gmail.com>
2025-05-04 21:52:21 +0900

    テーブルの作成完了

---
+++ src/database/migrations/2025_05_04_201829_create_todos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('content', 20);
+            // 下記でcreated_atとupdated_atの２つをカラムに追加できる
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
